9c6ae78c33c083aa24ca670bcbd9178e6ce97abd
Luis Irizarry <lirizarry6178@gmail.com>
2024-07-29 18:14:36 -0400

    Debugged Js Issues

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ function randomRGB() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    return `rgb(${r},${g},${b})`;
+    return `rgb(${r},${g},${b})`
 }
 
-const letter = document.querySelector(".letter");
+const letters = document.querySelectorAll(".letter");
 const intervalId = setInterval(function() {
     for (let letter of letters){
         letter.style.color = randomRGB();
